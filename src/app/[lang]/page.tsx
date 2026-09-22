7630f02895ac1f8e1b7539cb238e161c4a31d3dc
Yeeeraaal <ay.rodriguezg1@uniandes.edu.co>
2026-09-22 18:28:53 -0500

    ll

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,14 +1,8 @@
 import { getDictionary } from './dictionaries'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
-import type LayoutProps from "next";
 import Link from 'next/link'
 
-
-
 interface Razas {
-  name: string
-  image: string
+  message: string
 }
 
 async function getRazas(): Promise<Razas[]> {
@@ -17,42 +11,42 @@ async function getRazas(): Promise<Razas[]> {
 
 
   const razas = await Promise.all(
-    data.results.map(async (razas: { name: string; url: string }) => {
-      const DogRes = await fetch(razas.url)
+    data.results.map(async (razas: { message: string }) => {
+      const DogRes = await fetch(razas.message)
       const DogData = await DogRes.json()
 
-      
+
       return {
-        name: DogData.name,
-        image: DogData.sprites.front_default
+        message: DogData.message
       }
     })
-)
+  )
 
-return razas}
+  return razas
+}
 
 
-export default async function Page(){
+export default async function Page() {
   //parámetro dinámico del idioma
-    const dict = await getDictionary()
+  const dict = await getDictionary()
   //consumo de la API
   const razas = await getRazas()
 
   return (
     <main className="p-8 max-w-6xl mx-auto">
-      {/* Título desde el diccionario */}
-      
+     
+
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols- gap-6">
         {razas.map((raza) => (
           <div
-            key={raza.name}
+            key={raza.message}
             className="p-6 bg-white border border-gray-200 rounded-2xl shadow-md hover:shadow-xl transition-all duration-300 flex flex-col items-center text-center group"
           >
             <div className="w-32 h-32 mb-4 flex items-center justify-center bg-gray-50 rounded-full p-2 group-hover:scale-110 transition-transform">
               <img
-                src={raza.image}
-                alt={raza.name}
+                src={raza.message}
+                alt={raza.message}
                 className="w-full h-full object-contain"
               />
             </div>
@@ -60,19 +54,19 @@ export default async function Page(){
 
             {/* Nombre dinámico traído de la API */}
             <h2 className="text-xl font-bold text-gray-800 capitalize mt-1 mb-4">
-              {raza.name}
+              {raza.message}
             </h2>
 
-            {/* Texto traducido en el botón */}
-        <Link
-              href={`/pokemon/${raza.name}`}
+            
+            <Link
+              href={`/pokemon/${raza.message}`}
               className="w-full mt-auto bg-red-500 hover:bg-red-600 text-white font-medium py-2 px-4 rounded-xl text-center block"
             >
               detalle
             </Link>
           </div>
         ))}
-         </div>
+      </div>
     </main>
   )
 }
